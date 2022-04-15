8d04f56bafda96313571725080ccc40925d6b81e
Brenna Lavin <lavinbrenna@gmail.com>
2022-04-15 10:17:00 -0700

    update solution 2, add solution 3

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 
 // Output: "Jasmine%20Ann%20Jones"
 
+//non recursive
 let string = "Jasmine Ann Jones";
 function makeUrl(string){
   let stringToReplace = string.split('');
@@ -18,8 +19,38 @@ function makeUrl(string){
       urlString.push(stringToReplace[i]);
     }
     else{
-      urlString.push('%');
+      urlString.push('%20');
     }
   }
   return urlString.join('');
+}
+//recursive
+const recurseReverse = (string) => {
+  if (string === " ") {
+    return "%20";
+  } else {
+    return recurseReverse(string.substring(1)) + string[0];
+  }
+}
+
+
+// Question #2: Array Deduping
+// Write an algorithm that removes duplicates from an array. Do not use a function like filter() to solve this. Once you have solved the problem, demonstrate how it can be solved with filter(). Solve the problem with and without recursion.
+
+// Example
+// Output: [7, 9, "hi", 12, 53]
+let nums = [7, 9, "hi", 12, "hi", 7, 53];
+
+function setArray(arr){
+let newArray = [...new Set(arr)];
+console.log(newArray);
+}
+
+//with filter
+
+function filterArray(arr){
+  let newArray = arr.filter((element, index)=> {
+    return arr.indexOf(element) === index;
+});
+  console.log(newArray);
 }
