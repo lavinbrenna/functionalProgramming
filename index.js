d50e1d3e334cfc464a83f2330a46716f27f99aef
Brenna Lavin <lavinbrenna@gmail.com>
2022-04-15 12:08:13 -0700

    add solution for problem 5

--- index.js
+++ index.js
@@ -122,3 +122,34 @@ else{
 }
 isUnique(input1)
 isUnique(input2)
+
+// Question #5: Array Sorting
+// Write an algorithm that sorts an array without using the sort() method. There are many different sorting algorithms - take the time to read about the following:
+
+// Quick sort
+// Merge sort
+// Heap sort
+// Insertion sort
+// Bubble sort
+// Selection sort
+// You may implement any of the above algorithms (or your own) to solve the problem - as long as it doesn't use sort().
+
+// Example
+// Input: [9, 2, 7, 12]
+
+// Output: [2, 7, 9, 12]
+
+let nums = [9,2,7,12];
+function selectionSort(array) {
+  for (let currentIndex = 0; currentIndex < array.length; currentIndex++) {
+    for (let indexToCompare = currentIndex+1; indexToCompare < array.length; indexToCompare++) {
+      if (array[currentIndex] > array[indexToCompare]) {
+        const higherValue = array[currentIndex];
+        array[currentIndex] = array[indexToCompare];
+        array[indexToCompare] = higherValue;
+      }
+    }
+  }
+  return array;
+}
+selectionSort(nums)
